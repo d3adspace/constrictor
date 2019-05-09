85362fc29df7511a639d690b70e7ef13b021c148
Ruby Hale <ruby@d3adspace.de>
2019-05-09 16:09:31 +0200

    Add async UDP and TCP reachability checks.

--- tcp/src/main/java/de/d3adspace/constrictor/tcp/TCPReachability.java
+++ tcp/src/main/java/de/d3adspace/constrictor/tcp/TCPReachability.java
@@ -1,11 +1,20 @@
 package de.d3adspace.constrictor.tcp;
 
+import de.d3adspace.constrictor.core.thread.NamedThreadFactory;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
 
 public class TCPReachability {
 
+    private static final int THREAD_COUNT = 4;
+    private static final String THREAD_NAME_PREFIX = "TCP Reachability Check Thread";
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(THREAD_COUNT, new NamedThreadFactory(THREAD_NAME_PREFIX));
     private static final int DEFAULT_TIMEOUT = 4000;
 
     /**
@@ -23,5 +32,30 @@ public class TCPReachability {
         } catch (IOException ignore) {
             return false;
         }
+    }
+
+    /**
+     * Check if you can reach the given host at the given port via TCP.
+     *
+     * @param host The host.
+     * @param port The port.
+     * @param consumer The consumer for the result.
+     */
+    public static void checkReachability(String host, int port, Consumer<Boolean> consumer) {
+        EXECUTOR_SERVICE.execute(() -> {
+            boolean isReachable = checkReachability(host, port);
+            consumer.accept(isReachable);
+        });
+    }
+
+    /**
+     * Check if you can reach the given host at the given port via TCP.
+     *
+     * @param host The host.
+     * @param port The port.
+     * @return The future of the result.
+     */
+    public static Future<Boolean> checkReachabilityWithFuture(String host, int port) {
+        return EXECUTOR_SERVICE.submit(() -> checkReachability(host, port));
     }
 }
--- udp/src/main/java/de/d3adspace/constrictor/udp/UDPReachability.java
+++ udp/src/main/java/de/d3adspace/constrictor/udp/UDPReachability.java
@@ -1,12 +1,21 @@
 package de.d3adspace.constrictor.udp;
 
+import de.d3adspace.constrictor.core.thread.NamedThreadFactory;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
 
 public class UDPReachability {
 
+    private static final int THREAD_COUNT = 4;
+    private static final String THREAD_NAME_PREFIX = "UDP Reachability Check Thread";
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(THREAD_COUNT, new NamedThreadFactory(THREAD_NAME_PREFIX));
     private static final int DEFAULT_TIMEOUT = 1500;
     private static final byte[] EMPTY_BYTE_BUFFER = new byte[0];
     private static final DatagramPacket EMPTY_PACKET = new DatagramPacket(EMPTY_BYTE_BUFFER, EMPTY_BYTE_BUFFER.length);
@@ -20,7 +29,6 @@ public class UDPReachability {
      */
     public static boolean checkReachability(String host, int port) {
         try (DatagramSocket datagramSocket = new DatagramSocket()) {
-            long now = System.currentTimeMillis();
             datagramSocket.setSoTimeout(DEFAULT_TIMEOUT);
             datagramSocket.connect(new InetSocketAddress(host, port));
             datagramSocket.send(EMPTY_PACKET);
@@ -30,5 +38,30 @@ public class UDPReachability {
         } catch (IOException ignore) {
             return false;
         }
+    }
+
+    /**
+     * Check if you can reach the given host at the given port via UDP.
+     *
+     * @param host The host.
+     * @param port The port.
+     * @param consumer The consumer for the result.
+     */
+    public static void checkReachability(String host, int port, Consumer<Boolean> consumer) {
+        EXECUTOR_SERVICE.execute(() -> {
+            boolean isReachable = checkReachability(host, port);
+            consumer.accept(isReachable);
+        });
+    }
+
+    /**
+     * Check if you can reach the given host at the given port via UDP.
+     *
+     * @param host The host.
+     * @param port The port.
+     * @return The future of the result.
+     */
+    public static Future<Boolean> checkReachabilityWithFuture(String host, int port) {
+        return EXECUTOR_SERVICE.submit(() -> checkReachability(host, port));
     }
 }
